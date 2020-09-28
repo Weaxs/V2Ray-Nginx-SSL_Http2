1580565ebedbaa3bd2d41e1a27438de9b3130628
Weaxs <459312872@qq.com>
2020-09-28 23:07:05 +0800

    Update V2ray安装.sh

--- V2ray安装.sh
+++ V2ray安装.sh
@@ -3,3 +3,8 @@ bash <(curl -s -L https://git.io/v2ray.sh)
 
 # 官方安装需要用 yum 或 apt-get，自行安装 unzip 和 daemon
 bash <(curl -L -s https://install.direct/go.sh)
+
+
+# 注意：在设置端口号的时候必须和nginx配置中的代理的端口一样
+# 注意：在设置path的时候必须和nginx配置中的路由path一致
+# 注意：如果不能在安装的时候修改path，需要到/etc/v2ray目录下修改config.json文件中的path
